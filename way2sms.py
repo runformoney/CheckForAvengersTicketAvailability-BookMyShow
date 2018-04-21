bb77ff4992a30dd4cddb4a5d2799486c87776320
Rukhshan Rahman <36782664+runformoney@users.noreply.github.com>
2018-04-21 21:52:26 +0530

    Update way2sms.py

--- way2sms.py
+++ way2sms.py
@@ -1,143 +1,161 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class sms:
 
-	def __init__(self,username,password):
+    def __init__(self, username, password):
+        '''
+........Takes username and password as parameters for constructors
+........and try to log in
+........'''
 
-		'''
-		Takes username and password as parameters for constructors
-		and try to log in
-		'''
+        self.url = 'http://site24.way2sms.com/Login1.action?'
 
-		self.url='http://site24.way2sms.com/Login1.action?'
+        self.cred = {'username': username, 'password': password}
 
-		self.cred={'username': username, 'password': password}
+        self.s = requests.Session()  # Session because we want to maintain the cookies
 
-		self.s=requests.Session()			# Session because we want to maintain the cookies
+        self.s.headers['User-Agent'] = \
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
 
-		'''
-		changing s.headers['User-Agent'] to spoof that python is requesting
-		'''
+        self.q = self.s.post(self.url, data=self.cred)
 
-		self.s.headers['User-Agent']="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+        self.loggedIn = False  # a variable of knowing whether logged in or not
+        returnValue = False
+        if 'http://site24.way2sms.com/main.action' in self.q.url:  # http status 200 == OK
 
-		self.q=self.s.post(self.url,data=self.cred)
+            # print("Successfully logged in..!")
 
-		self.loggedIn=False				# a variable of knowing whether logged in or not
-		returnValue = False
-		if "http://site24.way2sms.com/main.action" in self.q.url:			# http status 200 == OK
+            self.loggedIn = True
+            returnValue = True
+        else:
 
-			#print("Successfully logged in..!")
-			self.loggedIn=True
-			returnValue = True
-		else:
+            # print("Can't login, once check credential..!")
 
-			#print("Can't login, once check credential..!")
-			self.loggedIn=False
-			returnValue = True
+            self.loggedIn = False
+            returnValue = True
 
-		self.jsid=self.s.cookies.get_dict()['JSESSIONID'][4:]	    # JSID is the main KEY as JSID are produced every time a session satrts
-		#return returnValue
+        self.jsid = (self.s.cookies.get_dict()['JSESSIONID'])[4:]  # JSID is the main KEY as JSID are produced every time a session satrts
 
-	def msgSentToday(self):
+        # return returnValue
 
-		'''
-		Returns number of SMS sent today as there is a limit of 100 messages everyday..!
-		'''
+    def msgSentToday(self):
+        '''
+........Returns number of SMS sent today as there is a limit of 100 messages everyday..!
+........'''
 
-		if self.loggedIn == False:
-			print("Can't perform since NOT logged in..!")
-			return -1
+        if self.loggedIn == False:
+            print ("Can't perform since NOT logged in..!")
+            return -1
 
-		self.msg_left_url='http://site24.way2sms.com/sentSMS?Token='+self.jsid
+        self.msg_left_url = 'http://site24.way2sms.com/sentSMS?Token=' \
+            + self.jsid
 
-		self.q=self.s.get(self.msg_left_url)
+        self.q = self.s.get(self.msg_left_url)
 
-		self.soup=BeautifulSoup(self.q.text,'html.parser')		#we want the number of messages sent which is present in the
+        self.soup = BeautifulSoup(self.q.text, 'html.parser')  # we want the number of messages sent which is present in the
 
-		self.t=self.soup.find("div",{"class":"hed"}).h2.text		# div element with class "hed" -> h2
+        self.t = self.soup.find('div', {'class': 'hed'}).h2.text  # div element with class "hed" -> h2
 
-		self.sent=0
+        self.sent = 0
 
-		for self.i in self.t:
+        for self.i in self.t:
 
-			if self.i.isdecimal():
+            if self.i.isdecimal():
 
-				self.sent=10*self.sent+int(self.i)
+                self.sent = 10 * self.sent + int(self.i)
 
-		return self.sent
+        return self.sent
 
-	def send(self,mobile_no,msg):
+    def send(self, mobile_no, msg):
+        '''
+........Sends the message to the given mobile number
+........'''
 
-		'''
-		Sends the message to the given mobile number
-		'''
+        if self.loggedIn == False:
+            print ("Can't perform since NOT logged in..!")
+            return False
 
-		if self.loggedIn == False:
-			print("Can't perform since NOT logged in..!")
-			return False
+        if len(msg) > 139 or len(mobile_no) != 10 \
+            or not mobile_no.isdecimal():  # checks whether the given message is of length more than 139
 
-		if len(msg)>139 or len(mobile_no)!=10 or not mobile_no.isdecimal():	#checks whether the given message is of length more than 139
+            return False  # or the mobile_no is valid
 
-			return False							#or the mobile_no is valid
+        self.payload = {  # inorder to visualize how I came to these payload,
+                          # must see the NETWORK section in Inspect Element
+                          # while messagin someone from your browser
+            'ssaction': 'ss',
+            'Token': self.jsid,
+            'mobile': mobile_no,
+            'message': msg,
+            'msgLen': '129',
+            }
 
-		self.payload={'ssaction':'ss',
-				'Token':self.jsid,					#inorder to visualize how I came to these payload,
-			        'mobile':mobile_no,					#must see the NETWORK section in Inspect Element
-       				 'message':msg,						#while messagin someone from your browser
-			        'msgLen':'129'
-       			     }
+        self.msg_url = 'http://site24.way2sms.com/smstoss.action'
 
-		self.msg_url='http://site24.way2sms.com/smstoss.action'
+        self.q = self.s.post(self.msg_url, data=self.payload)
 
-		self.q=self.s.post(self.msg_url,data=self.payload)
+        if self.q.status_code == 200:
 
-		if self.q.status_code==200:
+            return True
+        else:
 
-			return True
+            return False
 
-		else:
-			return False
+    def sendLater(  # Function for future SMS feature.
+        self,
+        mobile_no,
+        msg,
+        date,
+        time,
+        ):
 
-	def sendLater(self, mobile_no, msg, date, time):				#Function for future SMS feature.
-											#date must be in dd/mm/yyyy format
-											#time must be in 24hr format. For ex: 18:05
+                                            # date must be in dd/mm/yyyy format
+                                            # time must be in 24hr format. For ex: 18:05
 
-		if self.loggedIn == False:
-			print("Can't perform since NOT logged in..!")
-			return False
-		
-		if len(msg)>139 or len(mobile_no)!=10 or not mobile_no.isdecimal():
-			return False
+        if self.loggedIn == False:
+            print ("Can't perform since NOT logged in..!")
+            return False
 
-		dateparts = date.split('/')			#These steps to check for valid date and time and formatting
-		timeparts = time.split(':')
-		if int(dateparts[0])<1 or int(dateparts[0])>32 or int(dateparts[1])>12 or int(dateparts[1])<1 or int(dateparts[2])<2017 or int(timeparts[0])<0 or int(timeparts[0])>23 or int(timeparts[1])>59 or int(timeparts[1])<0:
-			return False
-		date = dateparts[0].zfill(2) + "/" + dateparts[1].zfill(2) + "/" + dateparts[2]
-		time = timeparts[0].zfill(2) + ":" + timeparts[1].zfill(2)
+        if len(msg) > 139 or len(mobile_no) != 10 \
+            or not mobile_no.isdecimal():
+            return False
 
-		self.payload={'Token':self.jsid,
-				'mobile':mobile_no,
-				'sdate':date,
-				'stime':time,
-				'message':msg,
-				'msgLen':'129'
-				}
+        dateparts = date.split('/')  # These steps to check for valid date and time and formatting
+        timeparts = time.split(':')
+        if int(dateparts[0]) < 1 or int(dateparts[0]) > 32 \
+            or int(dateparts[1]) > 12 or int(dateparts[1]) < 1 \
+            or int(dateparts[2]) < 2017 or int(timeparts[0]) < 0 \
+            or int(timeparts[0]) > 23 or int(timeparts[1]) > 59 \
+            or int(timeparts[1]) < 0:
+            return False
+        date = dateparts[0].zfill(2) + '/' + dateparts[1].zfill(2) \
+            + '/' + dateparts[2]
+        time = timeparts[0].zfill(2) + ':' + timeparts[1].zfill(2)
 
-		self.msg_url='http://site24.way2sms.com/schedulesms.action'
-		self.q=self.s.post(self.msg_url, data=self.payload)
+        self.payload = {
+            'Token': self.jsid,
+            'mobile': mobile_no,
+            'sdate': date,
+            'stime': time,
+            'message': msg,
+            'msgLen': '129',
+            }
 
-		if self.q.status_code==200:
-			return True
-		else:
-			return False
+        self.msg_url = 'http://site24.way2sms.com/schedulesms.action'
+        self.q = self.s.post(self.msg_url, data=self.payload)
 
-	def logout(self):
+        if self.q.status_code == 200:
+            return True
+        else:
+            return False
 
-		self.s.get('http://site24.way2sms.com/entry?ec=0080&id=dwks')
+    def logout(self):
 
-		self.s.close()								# close the Session
+        self.s.get('http://site24.way2sms.com/entry?ec=0080&id=dwks')
 
-		self.loggedIn=False
+        self.s.close()  # close the Session
+
+        self.loggedIn = False
+
